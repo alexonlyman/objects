fb437ed74af94d19969749e58307f9309ae5d807
Алекс <chu@mail.ru>
2022-10-27 22:36:33 +0300

    все задания

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
         System.out.println(book.getTitle());
         System.out.println(book.getNameAuthor());
-        System.out.println("book.getTitle() = " + book.getTitle());
+
         System.out.println(book.getPublicationYear());
         System.out.println(author.getFirstName());
         System.out.println(author.getSecondName());
 
         Book secondBook = new Book("Вий", "Николай Гоголь", 1835);
         Author secondAuthor = new Author("Николай", "Гоголь");
+        System.out.println(book.getTitle());
 
     }
 }
