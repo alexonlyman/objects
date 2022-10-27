cca44359ed15d99a51f47a5c7c9b98fd5aafda44
Алекс <chu@mail.ru>
2022-10-27 22:27:00 +0300

    все задания

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,33 @@
 public class Book {
+    private String title;
+    private String nameAuthor;
+    private int publicationYear;
+
+    public  Book (String title, String nameAuthor, int publicationYear ){
+
+        this.title = title;
+        this.nameAuthor = nameAuthor;
+        this.publicationYear = publicationYear;
+
+    }
+
+    public String getTitle() {
+        return this.title;
+
+    }
+
+    public String getNameAuthor() {
+        return this.nameAuthor;
+
+    }
+
+    public int getPublicationYear() {
+        return this.publicationYear;
+    }
+
+    public void setTitle(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
     public static void main(String[] args) {
+        Book book = new Book("Морфий", "Михаил Булгаков", 1926);
+        Author author = new Author("Михаил","Булгаков");
+
+        System.out.println(book.getTitle());
+        System.out.println(book.getNameAuthor());
+        System.out.println("book.getTitle() = " + book.getTitle());
+        System.out.println(book.getPublicationYear());
+        System.out.println(author.getFirstName());
+        System.out.println(author.getSecondName());
+
+        Book secondBook = new Book("Вий", "Николай Гоголь", 1835);
+        Author secondAuthor = new Author("Николай", "Гоголь");
 
     }
 }
